6b509a2e765ecbff54a20bddcf546a78bb34b424
Jithin George <c.jithingeorge@gmail.com>
2025-09-30 10:34:01 +0530

    Initial commit of CredSaver_Web

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "credsaver-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/CredSaver_Web.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
